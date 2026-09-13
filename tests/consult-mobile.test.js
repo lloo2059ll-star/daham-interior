ea21c91369d811075c88122ff25dedd369ddcc41
lloo2059ll-star <lloo2059ll@gmail.com>
2026-09-14 01:33:57 +0900

    Open updated consultation page from ERP navigation

--- tests/consult-mobile.test.js
+++ tests/consult-mobile.test.js
@@ -5,7 +5,7 @@ const assert=require('node:assert/strict');
 test('mobile ERP offers direct consultation access and consultation detail navigation',()=>{
   const consult=fs.readFileSync(path.join(__dirname,'../consult.html'),'utf8');
   const erp=fs.readFileSync(path.join(__dirname,'../erp.html'),'utf8');
-  assert.match(erp,/class="mobile-consult-entry" href="consult.html"/);
+  assert.match(erp,/class="mobile-consult-entry" href="consult\.html\?v=20260914-phone"/);
   assert.match(consult,/class="consult-mobile-nav"/);
   assert.match(consult,/id="consult-survey"/);
   assert.match(consult,/scrollIntoView\(\{block:'start'\}\)/);
